9f6bf5d7dd3333e732b6b2510699faafaf7eca1e
KeenanNorton <knorton@andrew.cmu.edu>
2023-03-25 16:19:24 -0400

    added tank 1 as a test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from tankClass import Tank
 
 pygame.init()
 width = 600
@@ -7,6 +8,9 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
+tank1 = Tank(50, 50, 200, 200, (255,255,255))
+tank_group = pygame.sprite.Group()
+tank_group.add(tank1)
 
 #from https://www.pygame.org/docs/
 while running:
@@ -18,6 +22,9 @@ while running:
 
     pygame.display.flip()
 
+    tank_group.draw(screen)
+
     clock.tick(60)
+
 
 pygame.quit()
