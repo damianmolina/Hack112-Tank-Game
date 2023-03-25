0808f4cfcba1a284f1ab24ac9530589466962cbf
KeenanNorton <knorton@andrew.cmu.edu>
2023-03-25 15:57:00 -0400

    initializes game and screen

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import pygame
 
+#Start of the game
+pygame.init()
+clock = pygame.time.Clock()
+
+#Screen for the game
+width = 600
+height = 600
+screen = pygame.display.set_mode((width, height))
