d1cfb80e0df6807209323ff3251b20be6df4ae02
voispugl <jvoisin@andrew.cmu.edu>
2023-03-25 15:59:30 -0400

    AhhhhAhhahHAhHAhHAHA

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pygame
 
+pygame.init()
+width = 600
+height = 600
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
