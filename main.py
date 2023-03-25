136908591d631a360fb90f1c7c9cc45d079fc029
voispugl <jvoisin@andrew.cmu.edu>
2023-03-25 16:04:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 running = True
 
+
+#from https://www.pygame.org/docs/
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("purple")
+    screen.fill("red")
 
     pygame.display.flip()
 
