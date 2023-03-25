255e1f2644136c4274eb098458d215e51d09b8e9
voispugl <jvoisin@andrew.cmu.edu>
2023-03-25 18:00:02 -0400

    movement

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ running = True
 screen.fill("red")
 
 #tank group
-tank1 = Tank(50, 50, 200, 200, (255,255,255), 2)
+tankWidth, tankHeight = 50, 50
+tank1 = Tank(tankWidth, tankHeight, 200, 200, (255,255,255), 2)
 tank_group = pygame.sprite.Group()
 tank_group.add(tank1)
 xSpeed, ySpeed = 10, 10
@@ -24,20 +25,21 @@ while running:
             running = False
     pygame.display.flip()
 
-    #draw tank
-    tank_group.draw(screen)
-
     #move tank from https://www.geeksforgeeks.org/python-moving-an-object-in-pygame/
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and tank1.rect.center[0] >= 0:
+    if keys[pygame.K_LEFT] and tank1.rect.center[0] - tankWidth >= 0:
         tank1.moveTank(xSpeed * -1, 0)
-    if keys[pygame.K_RIGHT] and tank1.rect.center[0] <= width:
+    elif keys[pygame.K_RIGHT] and tank1.rect.center[0] + tankWidth <= width:
         tank1.moveTank(xSpeed, 0)
-    elif keys[pygame.K_UP] and tank1.rect.center[1] > 0:
+    elif keys[pygame.K_UP] and tank1.rect.center[1] - tankHeight > 0:
         tank1.moveTank(0, ySpeed * -1)
-    elif keys[pygame.K_DOWN] and tank1.rect.center[1]  < height:
+    elif keys[pygame.K_DOWN] and tank1.rect.center[1] + tankHeight < height:
         tank1.moveTank(0, ySpeed)
-    screen.fill("red") #THIS FUCKS SHIT UP
+
+    #draw tank
+    screen.fill("red")
+    tank_group.draw(screen)
+
     clock.tick(60)
 
 pygame.quit()
