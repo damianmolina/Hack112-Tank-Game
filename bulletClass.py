69300f4f32079200259713fd41e3ac35c9ec1bf4
Mishos-untied <mvalexan@andrew.cmu.edu>
2023-03-25 18:26:49 -0400

    Update bulletClass.py

--- bulletClass.py
+++ bulletClass.py
@@ -1,14 +1,22 @@
+import math
 class Bullet:
-    bulletCount = 0
+    bulletList = []
     def __init__(self, cx, cy, dir):
-        if Bullet.bulletCount <= 2:
-            self.cx = cx
-            self.cy = cy
-            self.dir = dir
-            self.hasBounced = False
-            self.steps = 0
-            Bullet.bulletCount += 1
-            self.destory = False
+        self.cx = cx
+        self.cy = cy
+        self.dir = dir
+        self.hasBounced = False
+        self.steps = 0
+        self.destroy = False
+        Bullet.bulletList.append(self)
+    
+    def __repr__(self):
+        return f'Bullet(cx={self.cx}, cy={self.cy})'
+    
+    def __eq__(self, other):
+        return (isinstance(other, Bullet) and 
+                self.cx == other.cx and 
+                self.cy == other.cy)
     
     def move(self):
         if self.dir == 1:
@@ -40,25 +48,9 @@ class Bullet:
             else:
                 self.cy += 1
     
-    def onStep(self):
+    def step(self):
         self.steps += 1
         self.move()
 
-    def hasCollided(self):
-        if not 25 < self.cx < 575:
-            if not self.hasBounced:
-                self.hasBounced = True
-                self.dir = -self.dir
-            else:
-                self.destroy = True
-                Bullet.bulletCount -= 1     
-        if not 125 < self.cy < 575:
-            if not self.hasBounced:
-                self.hasBounced = True
-                self.dir = -self.dir  
-            else:
-                self.destroy = True
-                Bullet.bulleCount -= 1         
 
 
-            
