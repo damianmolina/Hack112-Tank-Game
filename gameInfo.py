5bf5ccfd7e6d34b88c6eb64f51566eb55f1516bd
KeenanNorton <knorton@andrew.cmu.edu>
2023-03-25 20:39:08 -0400

    game info text
    player scores

---
+++ gameInfo.py
@@ -0,0 +1,10 @@
+class GameInfo:
+    def __init__(self):
+        self.p1score = 0
+        self.p2score = 0
+        self.gamesPlayed = 0
+
+    def reset(self):
+        self.p1score = 0
+        self.p2score = 0
+        self.gamesPlayed = 0
